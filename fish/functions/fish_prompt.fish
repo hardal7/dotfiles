e69031850d7cd1f6ccc09b637ac4f3422971b02c
hardal <ozdilpoyraz23@gmail.com>
2023-11-19 16:01:27 +0300

    Switch to hyprland

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,28 +2,13 @@ function fish_prompt
     if not set -q VIRTUAL_ENV_DISABLE_PROMPT
         set -g VIRTUAL_ENV_DISABLE_PROMPT true
     end
-    set_color yellow
-    printf '%s' $USER
-    set_color normal
-    printf ' at '
-
-    set_color magenta
-    echo -n (prompt_hostname)
-    set_color normal
-    printf ' in '
-
-    set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
-    set_color normal
-
     set_color blue
+    printf '%s' (prompt_pwd)
+    set_color magenta
     printf '%s' (fish_git_prompt)
-
-    # Line 2
-    echo
     if test -n "$VIRTUAL_ENV"
         printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
     end
-    printf '➤ '
-    set_color normal
+    set_color green
+    printf ' $ '
 end
