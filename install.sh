51a460e9f3d29022e8455b488b7335038c9393f0
hardal <ozdilpoyraz23@gmail.com>
2023-11-19 16:01:27 +0300

    Switch to hyprland

--- install.sh
+++ install.sh
@@ -4,20 +4,17 @@ echo -e "\033[0;31mUpdating system files...\033[0m"
 pacman -Syyu --noconfirm
 
 echo -e "\033[0;31mInstalling yay...\033[0m"
-sudo -u hardal bash -c 'cd && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si --noconfirm && cd && rm -rf yay-bin'
+sudo -u hardal bash -c 'cd ~ && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si --noconfirm && cd ~ && rm -rf yay-bin'
 
 echo -e "\033[0;31mInstalling packages...\033[0m"
-sudo -u hardal bash -c 'xargs yay -S --noconfirm --needed <packages.txt' 
+sudo -u hardal bash -c 'cat packages.txt | yay -S --noconfirm --needed -' 
 
-echo -e "\033[0;31mInstalling icon theme...\033[0m"
+echo -e "\033[0;31mSetting icon theme...\033[0m"
 echo -e '[Icon Theme]\nInherits=Qogir' > /usr/share/icons/default/index.theme
 
 echo -e "\033[0;31mMoving dotfiles...\033[0m"
-mv ~/.config ~/dotfilesbackup
 git clone https://github.com/hardal7/dotfiles ~/.config 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-mv ~/.config/.xinitrc ~/.xinitrc 
-mv ~/.config/chrome ~/chrome
 
 echo -e "\033[0;31mRebooting system...\033[0m"
 sleep 3 && reboot
