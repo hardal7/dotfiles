84dd6abd58f039992a5f12188a1a8c8843896f99
hardal <ozdilpoyraz23@gmail.com>
2023-11-25 21:18:26 +0300

    Minor configs

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,7 +3,6 @@ local opt = vim.opt
 -- Save file, clipboard, change directory
 opt.undofile = true
 opt.clipboard = "unnamedplus"
-opt.autochdir = true
 
 -- Colors
 opt.termguicolors = true
@@ -19,7 +18,6 @@ opt.softtabstop = 2
 opt.showmode = false
 opt.laststatus = 0
 opt.ruler = false
-vim.wo.fillchars='eob: '
 
 -- Search settings
 opt.incsearch = true
