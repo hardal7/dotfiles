3c5caad4d2116b9c89c7434d7ecd95fc6fc5c731
hardal <ozdilpoyraz23@gmail.com>
2023-11-28 20:49:23 +0300

    Switch to nvchad

---
+++ nvim/lua/custom/configs/overrides.lua
@@ -0,0 +1,43 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "lua",
+    "c",
+    "cpp",
+    "bash"
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    "lua-language-server",
+    "bash-language-server",
+    "clangd",
+    "clang-format"
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
