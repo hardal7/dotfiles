11436e15017b2a7941a10797f452e8578e2cc910
hardal <ozdilpoyraz23@gmail.com>
2023-12-02 22:53:33 +0300

    Add mountain theme and fix battery script

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Repository for my dotfiles. 🐱
 Before running the script make sure you:
 - Installed Arch using the [arch installation guide](https://wiki.archlinux.org/title/Installation_guide)
 - Created a user account with sudo privileges
-- Have fakeroot and a video driver installed on your system
+- Have [fakeroot] (https://archlinux.org/packages/core/x86_64/fakeroot) installed on your system
 
 Simply run:
 
--- hypr/battery.sh
+++ hypr/battery.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 while true; do
-    battery_info=$(acpi -b)
-    battery_status=$(echo "$battery_info" | awk '{print $3}')
-    battery_level=$(echo "$battery_info" | awk '{print $4}' | tr -d ',%')
-    
-    if [ "$battery_status" == "Discharging," ] && [ $battery_level -lt 20 ]; then
+    battery_level=$(cat /sys/class/power_supply/BAT1/capacity)
+    if [ $battery_level -lt 20 ]; then
         notify-send "Low Battery" "Battery level is $battery_level%"
     fi
     
-    sleep 60
+    sleep 2
 done
