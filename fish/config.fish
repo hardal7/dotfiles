06bb290b6ff0b78f86b142e1c5e4a7841dca4751
hardal <ozdilpoyraz23@gmail.com>
2023-12-30 20:16:51 +0300

    Change theme and use ncmpcpp

--- fish/config.fish
+++ fish/config.fish
@@ -11,14 +11,27 @@ if not set -q TMUX
     eval $TMUX
     tmux rename-window -t 1 'Shell'
     tmux send-keys -t 'Shell' 'clear && rxfetch' C-m
+
     tmux new-window -t base:2 -n 'Editor'
     tmux send-keys -t 'Editor' 'nvim' C-m
+
     tmux new-window -t base:3 -n 'Monitor'
     tmux send-keys -t 'Monitor' 'btm' C-m
     tmux split-window -h -c 2
     tmux resize-pane -R 15
     tmux send-keys -t 'Monitor' 'htop' C-m
-    tmux select-window -t :1
+
+    tmux new-window -t base:4 -n 'Music'
+    tmux send-keys -t 'Music' 'ncmpcpp --screen browser' C-m
+    tmux split-window -h -c 2
+    tmux send-keys -t 'Music' 'alsamixer -c 1' C-m
+    tmux split-window -v -c 2
+    tmux send-keys -t 'Music' 'cava' C-m
+    tmux select-pane -t 1
+    tmux split-window -v -c 2
+    tmux send-keys -t 'Music' 'sptlrx --current "bold" --before "104,faint,italic" --after "104,faint"' C-m
+
+    tmux select-window -t 1
     tmux attach-session -d -t base
 end
 
