9359ce22f7c46a07f3c2ad073ae78ae4d0f2333b
hardal <ozdilpoyraz23@gmail.com>
2024-07-08 22:17:28 +0300

    Neovim config and make switch to alacritty

---
+++ nvim/lua/options.lua
@@ -0,0 +1,28 @@
+local opt = vim.opt
+
+-- Save file, clipboard, change directory
+opt.undofile = true
+opt.clipboard = "unnamedplus"
+opt.autochdir = true
+
+-- Colors
+opt.termguicolors = true
+
+-- Indent settings
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
+
+-- Hide bottom and left bar
+opt.showmode = false
+opt.laststatus = 0
+opt.ruler = false
+vim.wo.fillchars='eob: '
+opt.signcolumn = 'no'
+
+-- Search settings
+opt.incsearch = true
+opt.ignorecase = true
+opt.hlsearch = false
